e05094e45ce5af17190e5b4fab20f4ebecfaa1f7
lskandrea <157403455+lskandrea@users.noreply.github.com>
2026-06-15 13:20:55 +0200

    Update URLs and instructor names in config.js
    Updated Google Form and Drive URLs for sharing. Modified class schedule to reflect new instructor names.

--- config.js
+++ config.js
@@ -5,18 +5,18 @@ window.MJSC_CONFIG = {
   phone: "00 00 00 00 00",
   googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=Lyc%C3%A9e%20des%20Iscles%20Manosque",
   googleCalendarEmbedUrl: "https://calendar.google.com/calendar/embed?src=fr.french%23holiday%40group.v.calendar.google.com&ctz=Europe%2FParis",
-  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjSfwFfQDwoCRDX1ryQ0OJIu_pEe0zPilvZBVaEA/viewform?embedded=true",
-  googleDriveFolderUrl: "https://drive.google.com/drive/folders/1xPzKkZ_tdigLaPH5XZI_ixVt4WO3Wf5f?usp=sharing",
+  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjSfwFfQDwoCRDX1ryQ0OJIu_pEe0zPilvZBVaEA/viewform?usp=sharing&ouid=101478686819998394524",
+  googleDriveFolderUrl: "https://drive.google.com/drive/folders/1xPzKkZ_tdigLaPH5XZI_ixVt4WO3Wf5f?usp=drive_link",
   facebookUrl: "https://www.facebook.com/",
   instagramUrl: "https://www.instagram.com/",
   youtubeUrl: "https://www.youtube.com/",
   youtubeVideoId: "",
   horaires: [
-    ["Lundi", "17h30 - 18h30", "Baby Judo", "2020 - 2021", "Professeur diplômé"],
-    ["Lundi", "18h30 - 19h45", "Poussins / Benjamins", "2012 - 2015", "Professeur diplômé"],
-    ["Lundi", "20h00 - 21h30", "Ados / Adultes", "2011 et avant", "Professeur diplômé"],
-    ["Mercredi", "14h00 - 15h00", "Éveil Judo", "2018 - 2019", "Professeur diplômé"],
-    ["Mercredi", "15h00 - 16h15", "Poussins / Benjamins", "2012 - 2015", "Professeur diplômé"],
-    ["Vendredi", "18h00 - 19h30", "Ados / Adultes", "2011 et avant", "Professeur diplômé"]
+    ["Lundi", "17h30 - 18h30", "Baby Judo", "2020 - 2021", "Serge Colin"]
+    ["Lundi", "18h30 - 19h45", "Poussins / Benjamins", "2012 - 2015", "Serge Colin"],
+    ["Lundi", "20h00 - 21h30", "Ados / Adultes", "2011 et avant", "Serge COlin "],
+    ["Mercredi", "14h00 - 15h00", "Éveil Judo", "2018 - 2019", "Serge Colin "],
+    ["Mercredi", "15h00 - 16h15", "Poussins / Benjamins", "2012 - 2015", " Serge cOlin"],
+    ["Vendredi", "19h30 - 21h00", "Ados / Adultes", "2011 et avant", "Serge colin "]
   ]
 };
