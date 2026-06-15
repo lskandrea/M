8bac11252b7d93a424d6ea5e415b734d3c65cf0e
lskandrea <157403455+lskandrea@users.noreply.github.com>
2026-06-15 11:50:07 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 // Remplace les valeurs ci-dessous par les vrais liens du club.
 window.MJSC_CONFIG = {
   clubName: "MJSC Judo Manosque",
-  address: "Dojo du club, Manosque",
+  address: "Lycée des iscles, Manosque",
   email: "contact@mjsc-judo.fr",
   phone: "+33400000000",
-  googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=dojo%20judo%20Manosque",
+  googleMapsUrl: "https://www.google.com/maps/place/Lyc%C3%A9e+Polyvalent+Les+Iscles/@43.8297843,5.7949508,17z/data=!3m1!4b1!4m6!3m5!1s0x12cbcdcc7292b373:0x821e658de074a450!8m2!3d43.8297843!4d5.7975311!16s%2Fg%2F1ypmn68cw?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D",
 
   // Google Calendar : rends le calendrier public puis copie le lien d'intégration iframe.
   googleCalendarEmbedUrl: "https://calendar.google.com/calendar/embed?src=fr.french%23holiday%40group.v.calendar.google.com&ctz=Europe%2FParis",
 
   // Google Forms : crée un formulaire puis colle ici l'URL /viewform?embedded=true
-  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/TON_ID_FORMULAIRE/viewform?embedded=true",
+  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjSfwFfQDwoCRDX1ryQ0OJIu_pEe0zPilvZBVaEA/viewform?usp=sharing&ouid=101478686819998394524",
 
   // Google Drive : dossier partagé pour documents d'inscription.
-  googleDriveFolderUrl: "https://drive.google.com/drive/folders/TON_DOSSIER_DRIVE",
+  googleDriveFolderUrl: "https://drive.google.com/drive/folders/1xPzKkZ_tdigLaPH5XZI_ixVt4WO3Wf5f?usp=drive_link",
 
   facebookUrl: "https://www.facebook.com/",
   instagramUrl: "https://www.instagram.com/",
